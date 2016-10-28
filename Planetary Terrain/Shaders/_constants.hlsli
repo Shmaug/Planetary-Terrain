d2e3d97bcbc276adc123c4c4051d5ec7920d8763
Trevor Hedstrom <trevorhedstrom@gmail.com>
2016-10-27 21:20:56 -0700

    begain effects

--- Planetary Terrain/Shaders/_constants.hlsli
+++ Planetary Terrain/Shaders/_constants.hlsli
@@ -1,4 +1,4 @@
-#define PI   3.14159265359
+#define PI 3.14159265359
 #define PI4 12.56637061436
 #define G 9.8
 
--- Planetary Terrain/Tools/Resources/Shader.cs
+++ Planetary Terrain/Tools/Resources/Shader.cs
@@ -6,37 +6,16 @@ using D3D11 = SharpDX.Direct3D11;
 
 namespace Planetary_Terrain {
     class Shader : IDisposable {
-        public D3D11.VertexShader VertexShader { get; private set; }
-        public D3D11.PixelShader PixelShader { get; private set; }
-        public ShaderSignature Signature { get; private set; }
-        public D3D11.InputLayout InputLayout { get; private set; }
+        D3D11.Effect Effect;
         
         public Shader(string file, D3D11.Device device, D3D11.DeviceContext context, params D3D11.InputElement[] inputElements) {
-            using (var byteCode = ShaderBytecode.FromFile(file + "_vs.cso")) {
-                Signature = ShaderSignature.GetInputSignature(byteCode);
-                VertexShader = new D3D11.VertexShader(device, byteCode);
+            using (var byteCode = ShaderBytecode.FromFile(file)) {
+                Effect = new D3D11.Effect(device, byteCode);
             }
-            using (var byteCode = ShaderBytecode.FromFile(file + "_ps.cso")) {
-                PixelShader = new D3D11.PixelShader(device, byteCode);
-            }
-            InputLayout = new D3D11.InputLayout(device, Signature, inputElements);
         }
-
-        public void Set(Renderer renderer) {
-            renderer.Context.InputAssembler.InputLayout = InputLayout;
-            renderer.Context.VertexShader.Set(VertexShader);
-            renderer.Context.PixelShader.Set(PixelShader);
-
-            renderer.Context.VertexShader.SetConstantBuffer(0, renderer.constantBuffer);
-            renderer.Context.PixelShader.SetConstantBuffer(0, renderer.constantBuffer);
-            renderer.Context.PixelShader.SetSampler(0, renderer.AnisotropicSampler);
-        }
-
+        
         public void Dispose() {
-            VertexShader.Dispose();
-            PixelShader.Dispose();
-            Signature.Dispose();
-            InputLayout.Dispose();
+            Effect.Dispose();
         }
     }
 }
